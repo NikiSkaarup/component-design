7031d62f4d073519b08247b42c266e229dcd3955
Niki Wix Skaarup <7352837+NikiSkaarup@users.noreply.github.com>
2022-12-18 01:49:22 +0100

    Initial commit

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	$: message = $page?.error?.message ?? 'Whoops!';
+</script>
+
+<svelte:head>
+	<title>{message}</title>
+</svelte:head>
+
+<div class="flex min-h-screen w-full flex-col items-center justify-center">
+	<div class="flex flex-col gap-4 p-4">
+		<h1 class="text-xl">An error occurred</h1>
+		<table>
+			<tr>
+				<td>Status :</td>
+				<td>{$page.status}</td>
+			</tr>
+			<tr>
+				<td>Message :</td>
+				<td>{message}</td>
+			</tr>
+		</table>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '$lib/assets/app.css';
+</script>
+
+<div class="min-h-screen min-w-[100vw] dark:bg-gray-900 dark:text-slate-100">
+	<slot />
+</div>
